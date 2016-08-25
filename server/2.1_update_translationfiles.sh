4e6a70f0e3dcaddb2fb50f64265ef6736893d669
Rebecka Gulliksson <rebecka.gulliksson@umu.se>
2016-08-25 18:45:23 +0200

    Remove unused files.

--- server/2.1_update_translationfiles.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-pybabel update -i messages.pot -d translations
--- server/2_generate_translationfiles.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-pybabel init -i messages.pot -d translations -l sv_se
-pybabel init -i messages.pot -d translations -l en
-pybabel init -i messages.pot -d translations -l sv
