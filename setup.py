2fe77134afddb8cb8e83e2f8e9febd66f6a17405
Rebecka Gulliksson <rebecka.gulliksson@umu.se>
2016-09-22 10:21:10 +0200

    Bump version: 2.0.0 → 2.0.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ALservice',
-    version='2.0.0',
+    version='2.0.1',
     description='',
     author='DIRG',
     author_email='dirg@its.umu.se',
